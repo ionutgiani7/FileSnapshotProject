71ae0ab3c7acf4ade358b4afbc4d36b997e2c790
Ionut <asd@gmail.com>
2024-04-09 21:19:24 +0300

    dsa
    Signed-off-by: Ionut <asd@gmail.com>

---
+++ TLAB7v2.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+void compareVersions(FILE *f){
+    char v1[100][100];
+    char v2[100][100];
+
+    //strcpy(v1,"");
+    //strcpy(v2,"");
+
+    printf("aici");
+    char line[100];
+    int i=0;
+    while(fgets(line,sizeof(line),f) != NULL){
+        if(strstr(line,"stop") != NULL){
+            break;
+        }
+        strcat(v1[i++],line);
+    }
+    i=0;
+    while(fgets(line,sizeof(line),f) != NULL){
+        if(strstr(line,"stop") != NULL){
+            break;
+        }
+        strcat(v2[i++],line);
+    }
+
+    int j;
+    for(j=0;j<=i;j++)
+       if(strcmp(v1[j],v2[j]) != 0){
+           printf("Fisierul s a modificat\n");
+       }
+    
+    for(j=0;j<=i;j++){
+        if(strcmp(v1[j],v2[j]) != 0){
+            printf("%s <->\n%s\n",v1[j],v2[j]);
+        }
+    }
+    
+
+    
+    
+    
+
+    
+    
+}
+
+
+void parcurgereRecursiva(FILE *f,char *nume){
+    DIR *dir;
+    struct dirent *in;
+    if((dir = opendir(nume)) == NULL){
+        perror("Eroare la opendir\n");
+        return;
+    }
+
+     while((in = readdir(dir)) != NULL){
+          if(in->d_type == DT_DIR){
+              if(strcmp(in->d_name,".") == 0 || strcmp(in->d_name,"..") == 0)
+                     continue;
+              else{
+                fprintf(f,"Director: %s\n",in->d_name);
+                char x[1000];
+                strcpy(x,nume);
+                strcat(x,"/");
+                strcat(x,in->d_name);
+                parcurgereRecursiva(f,x);
+              }
+          }
+          else{
+            fprintf(f,"Fisier: %s\n",in->d_name);
+          }
+        
+    }
+
+
+    closedir(dir);
+    
+}
+
+
+int main(int argv,char *argc[]){
+    FILE *f;
+    if(argv > 11){
+        printf("Prea multe argumente\n");
+        exit(-1);
+    }
+    int i;
+    for(i=1;;i++){
+        if(strcmp(argc[i],"-o") == 0)
+            break;
+    }
+    i++;
+    
+    printf("%s\n",argc[i]);
+    if((f = fopen(argc[i],"a+")) == NULL){
+        printf("Eroare la deschidere de fisier\n");
+        exit(-1);
+    }
+    int x;
+    printf("Daca vrei sa salvezi o versiune de fisiere , apasa tasta 1. Daca vrei sa compari , apasa tasta 2\n");
+    scanf("%d",&x);
+    if(x == 1){
+        parcurgereRecursiva(f,argc[3]);
+        fprintf(f,"stop\n");
+    }
+    if(x == 2){
+        compareVersions(f);
+    }
+
+    
+      
+
+
+
+
+
+}
